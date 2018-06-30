a3a88d95a274c4ffa608e0bbe40a9fc784cb10d1
Jurandir Silva <jurandirjdsilva@gmail.com>
2018-06-30 17:58:57 -0300

    Reestruturação.

--- send_email.py
+++ send_email.py
@@ -1,27 +1,24 @@
-from crypto import Encript
+from src.crypto import Encript
 import zipfile
 import os
+import glob
 
 def menu():
     print("Digite a mensagem que deseja enviar")
     msg = raw_input()
-    file = open('./texto.txt', 'w')
-    file.write(msg)
-    file.close()
 
     print("Coloque os anexos na pasta 'anexos', digite 1 para continuar:")
     raw_input()
-    jungle_zip = zipfile.ZipFile('./msg.zip', 'w')
-    jungle_zip.write('./texto.txt', compress_type=zipfile.ZIP_DEFLATED)
+    with zipfile.ZipFile('./msg.zip', 'w') as jungle_zip:
+        jungle_zip.writestr('message.txt', bytes(msg), compress_type=zipfile.ZIP_DEFLATED)
 
-    caminhos = [os.path.join('./anexos', nome) for nome in os.listdir('./anexos')]
-    arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
-    anexos = [arq for arq in arquivos if arq.lower()]
 
-    for file in anexos:
-        jungle_zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+        caminhos = glob.glob('./anexos/*')
+        anexos = [arq for arq in caminhos if os.path.isfile(arq)]
 
-    jungle_zip.close()
+        for file in anexos:
+            jungle_zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+
     enc = Encript()
     key = enc.encrypt_file('./msg.zip')
 
